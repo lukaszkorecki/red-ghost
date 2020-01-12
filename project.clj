2906cad6f84a3cab7aaa6dea2a9be82ca876034a
Łukasz Korecki <lukasz@korecki.me>
2020-01-12 01:37:34 +0000

    Prep for deployment to Clojars

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
 (defproject red-ghost "0.1.0-SNAPSHOT"
-  :description ""
+  :description "Component for message queue implenentation, powered by Carmine"
   :url "https://github.com/lukaszkorecki/red-ghost"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.5.0-alpha.1"]
                  [com.stuartsierra/component "0.4.0"]
-                 [com.taoensso/carmine "2.19.1"]]
-  :deploy-repositories {"clojars" {:sign-releases false}}
+                 [com.taoensso/carmine "2.19.1"]
+                 [org.clojure/tools.logging "0.5.0"]]
+
+  :deploy-repositories {"clojars" {:sign-releases false
+                                   :username :env/clojars_username
+                                   :password :env/clojars_password}}
+
   :min-lein-version "2.9.0"
   :profile {:dev {:resource-paths ["dev-resources"]
                   :dependencies [[ch.qos.logback/logback-classic "1.2.3"]]}})
